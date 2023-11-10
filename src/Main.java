f4c6f31bf92c42ff1fc052b56cc922b0e74f3825
MURAT <muratdura03@gmail.com>
2023-11-10 11:36:26 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Temperature deneme1 = new Temperature();
+        Temperature deneme2 = new Temperature(30);
+        Temperature deneme3 = new Temperature(deneme1);
+        System.out.println(deneme1.toString());
+        System.out.println(deneme2.toString());
+        System.out.println(deneme3.toString());
+        Temperature deneme4 = new Temperature(65);
     }
 }
---
+++ src/Temperature.java
@@ -0,0 +1,82 @@
+import java.util.Random;
+
+public class Temperature {
+    // Fields to store temperature measurements in Celsius, Fahrenheit, and Kelvin
+    private double celsiusMeasurement;
+    private double fahrenheitMeasurement;
+    private double kelvinMeasurement;
+
+    // Constants for the valid range of temperature measurements
+    private final double MIN_VALUE = -40.0;
+    private final double MAX_VALUE = 50.0;
+
+    // Default constructor initializes temperature measurements using random values
+    public Temperature() {
+        celsiusMeasurement = initializeTemperature();
+        fahrenheitMeasurement = convertFahrenheit();
+        kelvinMeasurement = convertKelvin();
+    }
+
+    // Parameterized constructor initializes temperature measurements with a specified Celsius value
+    public Temperature(double celsiusMeasurement) {
+        // Check if the provided Celsius value is within the valid range
+        if (isInRange(celsiusMeasurement)) {
+            this.celsiusMeasurement = celsiusMeasurement;
+            fahrenheitMeasurement = convertFahrenheit();
+            kelvinMeasurement = convertKelvin();
+        } else {
+            // Throw an exception if the value is outside the valid range
+            throw new IllegalArgumentException("Measurement outside the range (" + MIN_VALUE + "-" + MAX_VALUE + ")");
+        }
+    }
+
+    // Copy constructor creates a new Temperature object from an existing one
+    public Temperature(Temperature aTemperature) {
+        celsiusMeasurement = aTemperature.getCelsiusMeasurement();
+        fahrenheitMeasurement = aTemperature.getFahrenheitMeasurement();
+        kelvinMeasurement = aTemperature.getKelvinMeasurement();
+    }
+
+    // Private method to initialize temperature with a random value in the valid range
+    private double initializeTemperature() {
+        return truncateToTwoDecimals(new Random().nextDouble() * (MAX_VALUE - MIN_VALUE) + MIN_VALUE);
+    }
+
+    // Private method to convert Celsius to Fahrenheit
+    private double convertFahrenheit() {
+        return truncateToTwoDecimals(celsiusMeasurement * 9 / 5 + 32);
+    }
+
+    // Private method to convert Celsius to Kelvin
+    private double convertKelvin() {
+        return truncateToTwoDecimals(celsiusMeasurement + 273);
+    }
+
+    // Getter methods to retrieve temperature measurements
+    public double getCelsiusMeasurement() {
+        return celsiusMeasurement;
+    }
+
+    public double getFahrenheitMeasurement() {
+        return fahrenheitMeasurement;
+    }
+
+    public double getKelvinMeasurement() {
+        return kelvinMeasurement;
+    }
+
+    // Override toString method to provide a string representation of the Temperature object
+    public String toString() {
+        return String.format("Celsius: %.2f Fahrenheit: %.2f Kelvin: %.2f", getCelsiusMeasurement(), getFahrenheitMeasurement(), getKelvinMeasurement());
+    }
+
+
+    private static double truncateToTwoDecimals(double value) {
+        return Math.floor(value * 100) / 100;
+    }
+
+    // Private method to check if a value is within the valid range
+    private boolean isInRange(double value) {
+        return value >= MIN_VALUE && value <= MAX_VALUE;
+    }
+}
